e2760527ab06e4b33f646646a3f18d1f593b6663
Rahmi Nurdin <sanggarsenisulota12@gmail.com>
2017-10-30 07:05:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,5 +18,15 @@ folium.Marker([-7.770692, 110.380315], popup='<i>Fakultas Ekonomika dan Bisnis U
 folium.Marker([-7.770181, 110.378661], popup='<i>Grha Sabha Pramana UGM</i>').add_to(kelompok4)
 folium.Marker([-7.770049, 110.381663], popup='<i>Fakultas Hukum UGM</i>').add_to(kelompok4)
 
+folium.Marker([-7.7834434,110.3208186], popup='<i>Toko Sahabat</i>').add_to(kelompok4)
+folium.Marker([-7.7837716,110.3221678], popup='<i>Warung Makan Nasi Rames Bu Sri</i>').add_to(kelompok4)
+folium.Marker([-7.7837716,110.3216206], popup='<i>Rumah Makan Padang Murah</i>').add_to(kelompok4)
+folium.Marker([-7.7837357,110.322369], popup='<i>Sup Buah Dan Es Campur Cak Adi</i>').add_to(kelompok4)
+folium.Marker([-7.7837613,110.322894], popup='<i>Cafe Via</i>').add_to(kelompok4)
+folium.Marker([-7.7839414,110.3229101], popup='<i>Gessa Cake</i>').add_to(kelompok4)
+folium.Marker([-7.7838885,110.3229671], popup='<i>Chinese Food Kasih Karunia (KK)</i>').add_to(kelompok4)
+folium.Marker([-7.7848994,110.3230408], popup='<i>Mie Ayam " Bu Tini "</i>').add_to(kelompok4)
+folium.Marker([-7.785114,110.3230952], popup='<i>Soto & Bakso Pak Pagi</i>').add_to(kelompok4)
+folium.Marker([-7.7856827,110.3229463], popup='<i>Mava Masakan Padang</i>').add_to(kelompok4)
 
 kelompok4
